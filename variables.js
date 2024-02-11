257c0d97b61e738394be8deddd9d2e52028818e1
Apu563 <asarkar563@gmail.com>
2024-02-12 01:21:58 +0530

    try

---
+++ variables.js
@@ -0,0 +1,28 @@
+
+const a = 500
+let b = "Apu"
+var c = "ani"
+d = "qwerty"
+
+// a=600
+b= "APURBA"
+c ="ANI"
+d="asdfgh"
+
+// const is unchangable, remainings are changable.
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+// Print in a Table format
+console.table([a,b,c,d])
+
+// let is preferable, var is not so used nowadays.
+
+
+// In javascript we can define blank variable, which is undefined.
+
+let n
+console.log(n)
+
